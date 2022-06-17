1c9b5b108bfb795750e9c0fa27752f1fae11d607
Achutendra Singh <achu1997singh@gmail.com>
2022-06-18 00:30:49 +0530

    initial commit

---
+++ Week-5/jest/calculate.test.js
@@ -0,0 +1,13 @@
+const all = require('./5_4');
+
+
+test('testing sum',() => {``
+    expect(all.sum(10 , 2)).toBe(12);
+});
+test('testing difference',() => {``
+    expect(all.difference(10 , 2)).toBe(8);
+});
+test('testing multiply',() => {``
+    expect(all.product(10 , 2)).toBe(20);
+});
+
